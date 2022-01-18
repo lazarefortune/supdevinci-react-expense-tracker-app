346758449cdc53401191411976db81bf1d76a629
Lazare Fortune <lazarefortune@gmail.com>
2022-01-18 16:44:05 +0100

    Refactor: integration of Formik and Yup in the form (basic use)

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,14 +1,24 @@
 import React, { useContext, useState } from "react"
 import { GlobalContext } from "../context/GlobalState"
+import { useFormik } from "formik"
+import * as Yup from "yup"
+
+const validationSchema = Yup.object().shape({
+  text: Yup.string().required("La description est obligatoire").min(3, "La description doit avoir au minimum 3 caractères"),
+  amount: Yup.number().typeError("Veuillez saisir un montant valide").required("Le montant est obligatoire")
+})
 
 export const AddTransaction = () => {
-  const [text, setText] = useState("")
-  const [amount, setAmount] = useState(0)
 
   const { addTransaction } = useContext(GlobalContext)
 
-  const onSubmit = (e) => {
-    e.preventDefault()
+  const initialValues = {
+    text: "",
+    amount: 0
+  }
+
+  function expenseSubmit(formValues) {
+    const { text, amount } = formValues
 
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
@@ -19,28 +29,44 @@ export const AddTransaction = () => {
     addTransaction(newTransaction)
   }
 
+  const formik = useFormik({
+    initialValues,
+    onSubmit: expenseSubmit,
+    validationSchema
+  })
+
+  const { text, amount } = formik.values
+
   return (
     <>
       <h5>Add expense</h5>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="text">Description</label>
-          <input
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
-            className="form-control"
-          />
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <div className="form-group">
+            <label htmlFor="text">Description</label>
+            <input
+              type="text"
+              name="text"
+              { ...formik.getFieldProps("text")}
+              placeholder="Enter text..."
+              className="form-control"
+            />
+            {
+              formik.errors.text && formik.touched.text && <span className="text-danger">{formik.errors.text}</span>
+            }
+          </div>
           <div className="form-group my-2">
             <label htmlFor="amount">Amount (€)</label>
             <input
-              type="text"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
+              type="number"
+              name="amount"
+              { ...formik.getFieldProps("amount")}
               placeholder="Enter amount..."
               className="form-control"
             />
+            {
+              formik.errors.amount && formik.touched.amount && <span className="text-danger">{formik.errors.amount}</span>
+            }
           </div>
           <button className="btn btn-primary">Add expense</button>
         </div>
